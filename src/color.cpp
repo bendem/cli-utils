caa0584e806ea429e9e238ea4aeacf1a41e0ba43
bendem <online@bendem.be>
2016-08-06 23:19:52 +0200

    color: add regex support

--- src/color.cpp
+++ src/color.cpp
@@ -5,7 +5,7 @@
 const std::string BEFORE = ANSI_RED;
 const std::string AFTER  = ANSI_RESET;
 
-int color(str line, str search) {
+int color_string(str line, str search) {
     size_t current = 0;
     size_t pos = line.find(search);
     while (pos != std::string::npos) {
@@ -14,6 +14,17 @@ int color(str line, str search) {
         pos = line.find(search, current);
     }
     std::cout << line.substr(current) << std::endl;
+
+    return 0;
+}
+
+int color_regex(str line, std::regex search) {
+    std::string suffix = line;
+    for (std::sregex_iterator it(line.begin(), line.end(), search), end; it != end; ++it) {
+        std::cout << it->prefix() << BEFORE << it->str() << AFTER;
+        suffix = it->suffix();
+    }
+    std::cout << suffix << std::endl;
 
     return 0;
 }
@@ -29,5 +40,18 @@ int main(int argc, char const *argv[]) {
         std::cerr << "Missing search parameter, see --help for more information." << std::endl;
     }
 
-    return for_lines_in(std::cin, color, args.arguments[0]);
+    if (args.has_flag("regex")) {
+        std::regex search(args.arguments[0], std::regex::extended
+            | std::regex::optimize
+            | std::regex::nosubs
+            | std::regex::collate
+            | args.has_flag("case-insensitive")
+                ? std::regex::icase
+                : static_cast<std::regex_constants::syntax_option_type>(0));
+
+        return for_lines_in(std::cin, color_regex, search);
+    } else {
+        return for_lines_in(std::cin, color_string, args.arguments[0]);
+    }
+
 }
